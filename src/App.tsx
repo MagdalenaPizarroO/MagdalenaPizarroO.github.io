68dcf8e68e616898190b9e7a205da7b85f11c32a
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-07-10 18:51:24 -0400

    prueba configuracion gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import {
-  BotonesNavegacionCon,
-  GradientBackgroundCon,
-  PresentacionCon,
-} from "./components/AppElements";
-import {
-  FooterContainer,
-  FooterLink,
-  RedSpan,
-} from "./components/FooterElements";
-import Button from "@mui/material/Button";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <>
-        <GradientBackgroundCon>
-          <PresentacionCon>
-            Hello World!
-            <BotonesNavegacionCon>
-              <Link to="/about">
-                <Button variant="outlined">About</Button>
-              </Link>
-              <Link to="/work">
-                <Button variant="outlined">Work</Button>
-              </Link>
-              <Link to="/contact">
-                <Button variant="outlined">Contact</Button>
-              </Link>
-            </BotonesNavegacionCon>
-          </PresentacionCon>
-        </GradientBackgroundCon>
-
-        {/* Footer */}
-        <FooterContainer>
-          <>
-            <br />
-            Developed with <RedSpan>♥︎</RedSpan> by{" "}
-            <FooterLink
-              href="https://github.com/MagdalenaPizarroO"
-              target="_blank"
-              rel="noopener noreferred"
-            >
-              @Magdis
-            </FooterLink>
-          </>
-        </FooterContainer>
-      </>
-    </Router>
-  );
+    <BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/MagdalenaPizarroO.github.io/'}
+    >
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>Hello World</h1>
+              <Link to='about'>About Us</Link>
+            </div>
+          }
+        />
+        <Route path='/about' element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/MagdalenaPizarroO.github.io'
+    config.base = '/MagdalenaPizarroO.github.io/'
   }
 
   return config
