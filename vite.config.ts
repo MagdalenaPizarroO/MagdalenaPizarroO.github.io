ff8e23d8525a27d08616f72f313609db2b12c4cd
MagdalenaPizarroO <123502477+MagdalenaPizarroO@users.noreply.github.com>
2023-07-24 19:06:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/MagdalenaPizarroO.github.io/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/MagdalenaPizarroO.github.io/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/MagdalenaPizarroO.github.io/',
 })
